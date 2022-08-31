ad7cb655901846a3999e4eab98cb4f00d6619f15
blue32a <blue32a.jp@gmail.com>
2022-08-31 23:39:49 +0900

    mod・abs・pow・sqrtの実装

---
+++ src/python/Chapter_2/Code_2_2_1.py
@@ -0,0 +1,25 @@
+# 2.2.1 mod・abs・pow・sqrtの実装
+import math
+
+# 四則演算
+print(869 + 120) # 989
+print(869 - 120) # 749
+print(860 * 120) # 103200
+print(869 / 120) # 7.241666666666666
+print(869 // 120) # 7
+
+# 剰余（mod）
+print(8 % 5) # 3
+print(869 % 120) # 29
+
+# 絶対値（abs）
+print(abs(-45)) # 45
+print(abs(15)) # 15
+
+# 累乗（pow）
+print(10 ** 2) # 100
+print(3 ** 4) # 81
+
+# ルート（sqrt）
+print("%.5f" % math.sqrt(4.0)) # 2.00000
+print("%.5f" % math.sqrt(2.0)) # 1.41421
